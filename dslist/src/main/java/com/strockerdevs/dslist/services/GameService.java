8f8b95b50cefcc86ffdc81c0eb09f4a04c60aff5
Mauricio Stocker <caicara1010@gmail.com>
2025-01-22 13:56:36 -0300

    realizado a criação do endpoint de busca pelo id, criamos o service e controller e por fim  a consulta

--- dslist/src/main/java/com/strockerdevs/dslist/services/GameService.java
+++ dslist/src/main/java/com/strockerdevs/dslist/services/GameService.java
@@ -4,23 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.strockerdevs.dslist.dto.GameDTO;
 import com.strockerdevs.dslist.dto.GameMinDTO;
 import com.strockerdevs.dslist.entities.Game;
 import com.strockerdevs.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-    
 
     @Autowired
     private GameRepository gameRepository;
 
-    public List<GameMinDTO> findAll(){
-
-       List<Game> result =  gameRepository.findAll();
-       List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // o stream é responsavel por transforma um objeto em outro
-
-       return dto;
+    @Transactional(readOnly = true)// faz com que a busca ou a não escrica, so leitura seja mais rapida
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();// demos um get pois o id do game que traz é um objeto, assim ele encontra os dados necessarios
+        GameDTO dto = new GameDTO(result);
+        return dto;
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // o stream é responsavel por
+                                                                   // transforma um objeto em outro
+        return dto;
+    }
+
 }
