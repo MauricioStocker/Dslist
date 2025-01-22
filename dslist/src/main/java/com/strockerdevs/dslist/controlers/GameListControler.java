3e905fc485952e6c7bd7c8fae23ddc3ea47fb5a6
Mauricio Stocker <caicara1010@gmail.com>
2025-01-22 14:11:34 -0300

    realizado a configuaração do pesquisa lista contruindo todas as camadas para a requisição

--- dslist/src/main/java/com/strockerdevs/dslist/controlers/GameListControler.java
+++ dslist/src/main/java/com/strockerdevs/dslist/controlers/GameListControler.java
@@ -9,27 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.strockerdevs.dslist.dto.GameDTO;
+import com.strockerdevs.dslist.dto.GameListDTO;
 import com.strockerdevs.dslist.dto.GameMinDTO;
-
+import com.strockerdevs.dslist.services.GameListService;
 import com.strockerdevs.dslist.services.GameService;
 
 @RestController
-@RequestMapping(value = "/games")
-public class GameControler {
+@RequestMapping(value = "/lists")
+public class GameListControler {
 
     @Autowired
-    private GameService gameService;
+    private GameListService gameListService;
 
     @GetMapping // buscando todos os games registrados
-    public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
         return result;
     }
 
-    @GetMapping(value = "/{id}") // configurado no endpoint para que buscamos pelo id do jogo
+ /*   @GetMapping(value = "/{id}") // configurado no endpoint para que buscamos pelo id do jogo
     public GameDTO findById(@PathVariable Long id) {
         GameDTO result = gameService.findById(id);
         return result;
-    }
+    }*/ 
 
 }
