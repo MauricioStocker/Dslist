d3020699a910902ba89bf1131c95ae3ecd608323
Mauricio Stocker <caicara1010@gmail.com>
2025-01-28 15:09:53 -0300

    correção

--- src/main/java/com/strockerdevs/dslist/controlers/GameController.java
+++ src/main/java/com/strockerdevs/dslist/controlers/GameController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,7 +21,8 @@ import com.strockerdevs.dslist.services.GameService;
 import java.io.IOException;
 import java.util.List;
 
-@Controller // Usar @Controller para servir páginas HTML
+
+@RestController // Usar @Controller para servir páginas HTML
 public class GameController {
 
     @Autowired
--- src/main/java/com/strockerdevs/dslist/services/GameService.java
+++ src/main/java/com/strockerdevs/dslist/services/GameService.java
@@ -25,6 +25,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
+
+
 @Service
 public class GameService {
 
